a10eeb1a2e821db3eff140590f7ac2275df49596
Mirka <mirkanicolle@gmail.com>
2024-05-10 00:13:07 -0600

    prueba

--- prueba.py
+++ prueba.py
@@ -1,54 +1,12 @@
-class Grammar:
-    def __init__(self):
-        self.tokens = []
-        self.productions = {}
-
-    def add_token(self, token):
-        self.tokens.append(token)
-
-    def add_production(self, head, rules):
-        self.productions[head] = rules
-
-def parse_yapar_file(filepath):
-    grammar = Grammar()
-    current_production = None
-    reading_productions = False
-
-    with open(filepath, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if not line or line.startswith('/*') or line == '}' or line.startswith('print') or line == '{':
-                continue  # Ignorar líneas vacías, comentarios, cierres de bloques y prints
-            if line.startswith('%token'):
-                token = line.split()[1]
-                grammar.add_token(token)
-            elif line == '%%':
-                reading_productions = True
-                continue
-            elif reading_productions:
-                if line.startswith('rule'):
-                    current_production = line.split()[1].strip()  # Iniciar una nueva producción
-                    grammar.add_production(current_production, [])
-                elif current_production and (line.startswith('|') or line.endswith(';')):
-                    rules = line.split('|')
-                    for rule in rules:
-                        rule_clean = rule.strip().strip(';')
-                        if rule_clean:
-                            grammar.productions[current_production].append(rule_clean)
-                elif current_production and line:
-                    if line.startswith('{'):
-                        continue  # Ignorar apertura de bloques
-                    grammar.productions[current_production].append(line.strip())
-
-    return grammar
+from generador_lexico import read_yalex_file, generate_lexical_analyzer_code
 
 def main():
-    file_path = 'example.yalp'  # Asegúrate de que la ruta del archivo es correcta
-    grammar = parse_yapar_file(file_path)
-    tokens = grammar.tokens
-    productions = grammar.productions
-    print("Tokens:", tokens)
-    print("Productions:", productions)
+    file_path_yalex = 'hard_especificaciones.yalex'  # Asegúrate de que la ruta al archivo YALex sea correcta
+    rules = read_yalex_file(file_path_yalex)  # Carga las definiciones de tokens
 
-if __name__ == '__main__':
+    # Especifica un nombre de archivo de salida para el código del analizador léxico generado
+    output_file = 'lexical_analyzer.py'
+    generate_lexical_analyzer_code(rules, output_file)
+
+if __name__ == "__main__":
     main()
