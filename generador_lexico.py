45c527a5173e8afb3606892d95af20e2ceb7b02d
Mirka <mirkanicolle@gmail.com>
2024-05-12 12:30:47 -0600

    generador lexico

--- generador_lexico.py
+++ generador_lexico.py
@@ -6,44 +6,6 @@ from Thompson import regex_to_afn
 from Subconjuntos import afn_to_afd
 from Minimizacion import minimize_afd
 
-def read_yalex_file(file_path):
-    rules = {}
-    with open(file_path, 'r', encoding='utf-8') as file:
-        reading_rules = False
-        for line in file:
-            line = line.strip()
-            if line.startswith('%token'):
-                reading_rules = True
-            elif reading_rules and line != '%%':
-                if line.startswith('let') or line.startswith('rule'):
-                    parts = line.split('=')
-                    rule_name = parts[0].strip()
-                    rule_pattern = parts[1].strip().replace('{', '').replace('}', '').strip()
-                    rules[rule_name] = rule_pattern
-                elif line == '%%':
-                    reading_rules = False
-    return rules
-
-def process_regex_to_afd(regex):
-    postfix = to_postfix(regex) # Conversión de expresión regular en notación postfix
-    afn = regex_to_afn(postfix) # Generación de AFN
-    afd = afn_to_afd(afn) # Conversión de AFN a AFD
-    minimized_afd = minimize_afd(afd) # Minimización de AFD
-    return minimized_afd 
-
-def afd_to_dict(afd):
-    state_dict = {}
-    for state in afd.states:
-        state_id = id(state)
-        state_dict[state_id] = {'accept': state.accept, 'transitions': {}}
-        for symbol, states in state.transitions.items():
-            state_dict[state_id]['transitions'][symbol] = [id(s) for s in states]
-    start_state_id = id(afd.start_state)
-    return start_state_id, state_dict
-
-def generate_lexical_analyzer_code(tokens, output_file): #codigo fuente para analizador lexico
-    code = '''
-# -*- coding: utf-8 -*-
 class Token:
     def __init__(self, type_, value):
         self.type = type_
@@ -51,12 +13,29 @@ class Token:
 
     def __str__(self):
         return f"{self.type}: {self.value}"
-    
+
 class LexicalAnalyzer:
     def __init__(self, input_text=None):
         self.input = input_text
         self.current_position = 0
         self.total_length = len(input_text) if input_text else 0
+
+    def process_regex_to_afd(self, regex):  # Cambiado a método de instancia
+        postfix = to_postfix(regex)  # Conversión de expresión regular en notación postfix
+        afn = regex_to_afn(postfix)  # Generación de AFN
+        afd = afn_to_afd(afn)  # Conversión de AFN a AFD
+        minimized_afd = minimize_afd(afd)  # Minimización de AFD
+        return minimized_afd
+
+    def afd_to_dict(self, afd):  # Cambiado a método de instancia
+        state_dict = {}
+        for state in afd.states:
+            state_id = id(state)
+            state_dict[state_id] = {'accept': state.accept, 'transitions': {}}
+            for symbol, states in state.transitions.items():
+                state_dict[state_id]['transitions'][symbol] = [id(s) for s in states]
+        start_state_id = id(afd.start_state)
+        return start_state_id, state_dict
 
     def set_input(self, input_text):
         self.input = input_text
@@ -65,60 +44,68 @@ class LexicalAnalyzer:
 
     def get_next_token(self):
         while self.current_position < self.total_length:
-            if self.input[self.current_position].isspace():
+            current_char = self.input[self.current_position]
+
+            if current_char.isspace():
                 self.current_position += 1
                 continue
 
-            if self.input[self.current_position] == '/':
-                # Check for single line comment
+            if current_char == '/':
                 if self.current_position + 1 < self.total_length and self.input[self.current_position + 1] == '/':
                     self.skip_single_line_comment()
                     continue
-                # Check for multi line comment
                 elif self.current_position + 1 < self.total_length and self.input[self.current_position + 1] == '*':
                     self.skip_multi_line_comment()
                     continue
 
-            if self.input[self.current_position].isdigit():
+            if current_char.isdigit() or (current_char == '.' and self.current_position + 1 < self.total_length and self.input[self.current_position + 1].isdigit()):
                 return self.get_number()
 
-            if self.input[self.current_position].isalpha() or self.input[self.current_position] == '_':
+            if current_char.isalpha() or current_char == '_':
                 return self.get_identifier()
 
-            if self.input[self.current_position] in "+-*/()=.,;:{}[]<>!&|%^'@":
+            if current_char in "+-*/()=.,;:{}[]<>!&|%^'@":
                 return self.get_operator()
-        
+
+            self.current_position += 1
+
         return None
 
     def analyze(self, text):
+        self.set_input(text)  
         results = []
         errors = []
-        i = 0
-        while i < len(text):
-            if text[i] == '"' and (i + 1 < len(text) and text[i + 1] != "\\\\"):
-                result, i = self.handle_string(text, i)
+        while self.current_position < self.total_length:
+            if self.input[self.current_position] == '"' and (self.current_position + 1 < self.total_length and self.input[self.current_position + 1] != "\\\\"):
+                result, new_position = self.handle_string(self.input, self.current_position)
                 if result is not None:
                     results.append(Token("String", result))
-            elif text[i].isdigit():
-                num, i = self.handle_number(text, i)
-                results.append(Token("Number", num))
-            elif text[i].isalpha() or text[i] == '_':
-                ident, i = self.handle_identifier(text, i)
-                results.append(Token("Identifier", ident))
-            elif text[i].isspace():
-                i = self.handle_whitespace(text, i)
-            elif text[i] in "+-*/()=.,;:{}[]<>!&|%^'@":
-                op, i = self.handle_operator(text, i)
-                results.append(Token("Operator", op))
-            elif text[i] == '/' and i + 1 < len(text) and text[i + 1] == '/':
-                i = self.handle_single_line_comment(text, i + 2)
-            elif text[i] == '/' and i + 1 < len(text) and text[i + 1] == '*':
-                i = self.handle_multi_line_comment(text, i + 2)
+                self.current_position = new_position
+            elif self.input[self.current_position].isdigit():
+                result, new_position = self.handle_number(self.input, self.current_position)
+                results.append(Token("Number", result))
+                self.current_position = new_position
+            elif self.input[self.current_position].isalpha() or self.input[self.current_position] == '_':
+                result, new_position = self.handle_identifier(self.input, self.current_position)
+                results.append(Token("Identifier", result))
+                self.current_position = new_position
+            elif self.input[self.current_position].isspace():
+                self.current_position = self.handle_whitespace(self.input, self.current_position)
+            elif self.input[self.current_position] in "+-*/()=.,;:{}[]<>!&|%^'@":
+                result, new_position = self.handle_operator(self.input, self.current_position)
+                results.append(Token("Operator", result))
+                self.current_position = new_position
+            elif self.input[self.current_position] == '/' and self.current_position + 1 < self.total_length and self.input[self.current_position + 1] == '/':
+                self.current_position = self.handle_single_line_comment(self.input, self.current_position + 2)
+            elif self.input[self.current_position] == '/' and self.current_position + 1 < self.total_length and self.input[self.current_position + 1] == '*':
+                self.current_position = self.handle_multi_line_comment(self.input, self.current_position + 2)
             else:
-                errors.append(f"Unknown character: {text[i]} at position {i}")
-                i += 1
+                errors.append(f"Unknown character: {self.input[self.current_position]} at position {self.current_position}")
+                self.current_position += 1
         if errors:
             results.append(Token("Error", " ".join(errors)))
+        for token in results:
+            print(f"Token created: Type={token.type}, Value={token.value}") 
         return results
     
     def skip_whitespace(self):
@@ -126,34 +113,31 @@ class LexicalAnalyzer:
             self.current_position += 1
     
     def handle_string(self, text, i):
-        if text[i] == '"' and (i == 0 or text[i-1] != '\\\\'):  # Comienza una nueva cadena si no está escapada
+        if text[i] == '"' and (i == 0 or text[i-1] != '\\\\'):  
             i += 1
             start = i
             while i < len(text):
-                if text[i] == '"' and text[i-1] != '\\\\':  # Finaliza si encuentra una comilla no escapada
+                if text[i] == '"' and text[i-1] != '\\\\':  
                     return text[start:i], i + 1
                 i += 1
-            return text[start:], i  # Retorna lo que pueda si no encuentra el final
+            return text[start:], i
         return None, i
     
     def get_number(self):
         start_position = self.current_position
-        has_decimal = False
-        while self.current_position < self.total_length and (self.input[self.current_position].isdigit() or (self.input[self.current_position] == '.' and not has_decimal)):
-            if self.input[self.current_position] == '.':
-                has_decimal = True
+        while self.current_position < self.total_length and (self.input[self.current_position].isdigit() or self.input[self.current_position] == '.'):
             self.current_position += 1
-        value = self.input[start_position:self.current_position]
-        token_type = 'FLOAT' if '.' in value else 'NUMBER'
-        return {'type': token_type, 'value': value}
 
+        value = self.input[start_position:self.current_position]
+        return Token('FLOAT' if '.' in value else 'NUMBER', value)
 
     def get_identifier(self):
         start_position = self.current_position
         while self.current_position < self.total_length and (self.input[self.current_position].isalpha() or self.input[self.current_position] == '_'):
             self.current_position += 1
+
         value = self.input[start_position:self.current_position].upper()
-        return {'type': 'IDENTIFIER', 'value': value}
+        return Token('IDENTIFIER', value)
 
     def handle_number(self, text, i):
         num = ""
@@ -177,42 +161,38 @@ class LexicalAnalyzer:
     def get_operator(self):
         operator = self.input[self.current_position]
         self.current_position += 1
-        return {'type': 'OPERATOR', 'value': operator}
+        return Token('OPERATOR', operator)
 
     def handle_single_line_comment(self, text, i):
         while i < len(text) and text[i] != ' ':
             i += 1
         return i
 
-    def handle_multi_line_comment(self, text, i):
-        while i < len(text):
-            if text[i] == '*' and i+1 < len(text) and text[i+1] == '//':
-                return i + 2
-            i += 1
-        return i  
+    def handle_multi_line_comment(self):
+        self.current_position += 2  
+        while self.current_position + 1 < self.total_length:
+            if self.input[self.current_position] == '*' and self.input[self.current_position + 1] == '/':
+                self.current_position += 2  
+                return
+            self.current_position += 1
+        raise Exception("End of file reached without closing multi-line comment") 
 
     def skip_single_line_comment(self):
         while self.current_position < self.total_length and self.input[self.current_position] != '\\n':
             self.current_position += 1
+        self.current_position += 1
 
     def skip_multi_line_comment(self):
-        self.current_position += 2  # Skip the initial /*
+        self.current_position += 2
         while self.current_position + 1 < self.total_length:
             if self.input[self.current_position] == '*' and self.input[self.current_position + 1] == '/':
-                self.current_position += 2  # Skip past the */
-                break
+                self.current_position += 2
+                return
             self.current_position += 1
-'''
-    with open(output_file, 'w', encoding='utf-8') as file:
-        file.write(code)
-
-def main():
-    file_path_yalex = 'hard_especificaciones.yalex'  # Asegúrate de que la ruta al archivo YALex sea correcta
-    rules = read_yalex_file(file_path_yalex)  # Carga las definiciones de tokens
-
-    # Especifica un nombre de archivo de salida para el código del analizador léxico generado
-    output_file = 'lexical_analyzer.py'
-    generate_lexical_analyzer_code(rules, output_file)
-
-if __name__ == "__main__":
-    main()
+    
+    def handle_operator(self, text, i):
+        start = i
+        while i < len(text) and text[i] in "+-*/()=.,;:{}[]<>!&|%^'@":
+            i += 1
+        operator = text[start:i]
+        return operator, i
