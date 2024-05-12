2fb93e01c8a27d666f0fc1c660fa3c0aed06b255
Mirka <mirkanicolle@gmail.com>
2024-05-12 15:56:02 -0600

    interfaz

--- Intefaz.py
+++ Intefaz.py
@@ -1,88 +1,83 @@
+import graphviz
 import tkinter as tk
-from tkinter import filedialog, messagebox, scrolledtext
-import importlib.util
+from tkinter import filedialog, Text, messagebox
 
-from generador_lexico import read_yalex_file, generate_lexical_analyzer_code
-from generador_sintactico import parse_yapar_file, generate_sintactic_analyzer_code
+from generador_lexico import LexicalAnalyzer
+from generador_sintactico import Parser
+from generadores import parse_yapar_file, read_yalex_file
+from automaton import LR0Automaton
 
-class AnalysisApp(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+class MainApplication:
+    def __init__(self, root):
+        self.root = root
+        root.title("Laboratorio E")
+        root.geometry("800x600")
+        root.configure(bg='#b3d9ff')
 
-    def create_widgets(self):
-        self.load_lex_button = tk.Button(self, text="Load Lexer", command=self.load_yalex)
-        self.load_lex_button.pack(side="top")
+        self.output_area = Text(root, height=20, width=80, bg="#e6f2ff")
+        self.output_area.pack(pady=20)
 
-        self.load_syn_button = tk.Button(self, text="Load Parser", command=self.load_yapar)
-        self.load_syn_button.pack(side="top")
+        button_frame = tk.Frame(root, bg='#b3d9ff')
+        button_frame.pack(fill=tk.X, pady=10)
 
-        self.load_file_button = tk.Button(self, text="Load Text File", command=self.load_text_file)
-        self.load_file_button.pack(side="top")
+        open_file_button = tk.Button(button_frame, text="Cargar Archivo", font=('Helvetica', 12), padx=20, pady=10, fg="white", bg="#3399ff", command=self.open_file)
+        open_file_button.pack(side=tk.LEFT, padx=20, expand=True)
 
-        self.result_text = scrolledtext.ScrolledText(self, height=30, width=80)
-        self.result_text.pack(side="bottom")
+        run_automaton_button = tk.Button(button_frame, text="Generar Autómata LR(0)", font=('Helvetica', 12), padx=20, pady=10, fg="white", bg="#3399ff", command=self.generate_automaton)
+        run_automaton_button.pack(side=tk.LEFT, padx=20, expand=True)
 
-    def load_yalex(self):
-        filepath = filedialog.askopenfilename(filetypes=[("YALex files", "*.yalex"), ("All files", "*.*")])
-        if filepath:
-            tokens = read_yalex_file(filepath)
-            output_file = 'lexical_analyzer.py'
-            generate_lexical_analyzer_code(tokens, output_file)
-            self.load_module(output_file, 'lexical')
+        self.grammar = None  
 
-    def load_yapar(self):
-        filepath = filedialog.askopenfilename(filetypes=[("YAPar files", "*.yalp"), ("All files", "*.*")])
-        if filepath:
-            grammar = parse_yapar_file(filepath)
-            output_file = 'sintactic_analyzer.py'
-            generate_sintactic_analyzer_code(grammar, output_file)
-            self.load_module(output_file, 'sintactic')
+    def open_file(self):
+        filename = filedialog.askopenfilename(initialdir="/", title="Seleccionar Archivo", filetypes=(("text files", "*.txt"), ("all files", "*.*")))
+        if filename:
+            input_text = self.read_file(filename)
+            self.process_text(input_text)
 
-    def load_text_file(self):
-        filepath = filedialog.askopenfilename(filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
-        if filepath:
-            with open(filepath, 'r') as file:
-                text = file.read()
-            if self.lexical_analyzer:
-                tokens = self.lexical_analyzer.analyze(text)
-                self.display_tokens(tokens)
-                if self.sintactic_analyzer:
-                    self.run_sintactic_analysis(tokens)  
-            else:
-                messagebox.showerror("Error", "Lexical analyzer not generated.")
+    def process_text(self, input_text):
+        yalex_path = 'hard_especificaciones.yalex'
+        yapar_path = 'especificaciones_yapar.yalp'
 
-    def load_module(self, module_path, type):
-        spec = importlib.util.spec_from_file_location(module_path.replace('.py', ''), module_path)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        if type == 'lexical':
-            self.lexical_analyzer = module.LexicalAnalyzer()
-            self.result_text.insert(tk.END, "Lexical analyzer generated and loaded successfully.\n")
-        elif type == 'sintactic':
-            self.sintactic_analyzer = module.Parser()  
-            self.result_text.insert(tk.END, "Sintactic analyzer generated and loaded successfully.\n")
-    
-    def display_tokens(self, tokens):
-        self.result_text.insert(tk.END, "Tokens Found:\n")
-        for token in tokens:
-            self.result_text.insert(tk.END, f"{token.type}: {token.value}\n") 
+        lexical_rules = read_yalex_file(yalex_path)
+        grammar_rules = parse_yapar_file(yapar_path)
 
-    def run_sintactic_analysis(self, tokens):
-        if not self.sintactic_analyzer:
-            messagebox.showerror("Error", "Sintactic analyzer not loaded.")
-            return
-        self.sintactic_analyzer.set_tokens(tokens)  #
+        lexical_analyzer = LexicalAnalyzer(lexical_rules)
+        lexical_analyzer.set_input(input_text)
+        tokens = []
+        self.output_area.insert(tk.END, "Generated Tokens:\n")
+        while True:
+            token = lexical_analyzer.get_next_token()
+            if token is None:
+                break
+            tokens.append(token)
+            self.output_area.insert(tk.END, f"Token: Type={token.type}, Value={token.value}\n")
+
+        parser = Parser(tokens)
         try:
-            parsed_structure = self.sintactic_analyzer.parse_expression()
-            self.result_text.insert(tk.END, f"\nParsed structure:\n{parsed_structure}")
+            parse_tree = parser.parse()
+            self.output_area.insert(tk.END, "\nSyntactic Analysis:\n")
+            self.output_area.insert(tk.END, str(parse_tree) + "\n")
+            self.parse_tree = parse_tree  
         except Exception as e:
-            messagebox.showerror("Error", f"Syntactic analysis failed: {e}")
-            self.result_text.insert(tk.END, f"Error: {str(e)}\n")
+            self.output_area.insert(tk.END, f"Error parsing: {e}\n")
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = AnalysisApp(master=root)
-    app.mainloop()
+    def generate_automaton(self):
+        if self.grammar:
+            try:
+                automaton = LR0Automaton(self.grammar)
+                automaton.build_automaton()
+                automaton_output = automaton.visualize()
+                self.output_area.insert(tk.END, automaton_output + "\n")
+            except Exception as e:
+                messagebox.showerror("Error", f"Failed to generate automaton: {e}")
+        else:
+            messagebox.showerror("Error", "Grammar not loaded. Please load a file and validate syntax first.")
+
+    @staticmethod
+    def read_file(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+
+root = tk.Tk()
+app = MainApplication(root)
+root.mainloop()
