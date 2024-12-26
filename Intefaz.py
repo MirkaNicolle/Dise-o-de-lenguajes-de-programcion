bacc84887bd5e5e3c0c7ac6e5e615327b79b9518
Mirka <mirkanicolle@gmail.com>
2024-12-26 15:48:37 -0600

    Interfaz

--- Intefaz.py
+++ Intefaz.py
@@ -11,14 +11,14 @@ class LexicalApp:
 
         button_font = font.Font(family="Helvetica", size=12, weight="bold")
 
-        self.load_yalex_button = tk.Button(master, text="Load YALex Specifications", command=self.load_yalex, #boton yalex
+        self.load_yalex_button = tk.Button(master, text="Especificaciones YALex", command=self.load_yalex, #boton yalex
         bg='#D8BFD8', fg='black', font=button_font, relief='flat', bd=0,
         activebackground='#DA70D6', activeforeground='white')
         self.load_yalex_button.pack(pady=10, padx=10)
         self.load_yalex_button.bind("<Enter>", lambda e: e.widget.config(bg='#DA70D6')) 
         self.load_yalex_button.bind("<Leave>", lambda e: e.widget.config(bg='#D8BFD8'))  
 
-        self.load_text_button = tk.Button(master, text="Load Text File", command=self.load_text_file, #boton archivo a analizar
+        self.load_text_button = tk.Button(master, text=".txt a analizar", command=self.load_text_file, #boton archivo a analizar
         bg='#D8BFD8', fg='black', font=button_font, relief='flat', bd=0,
         activebackground='#DA70D6', activeforeground='white')
         self.load_text_button.pack(pady=10, padx=10)
@@ -43,14 +43,14 @@ class LexicalApp:
         lexical_analyzer = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(lexical_analyzer)
         self.analyzer = lexical_analyzer.LexicalAnalyzer()
-        self.result_text.insert(tk.END, "Analizador listo. Cargue un archivo de texto para analizar.\n")
+        self.result_text.insert(tk.END, "Analizador listo! Cargue un archivo de texto para analizar.\n")
 
     def load_text_file(self):
         filepath = filedialog.askopenfilename(filetypes=(("Text files", "*.txt"), ("All files", "*.*")))
         if not filepath:
             return
         if not self.analyzer:
-            messagebox.showerror("Error", "Please generate the lexical analyzer first.")
+            messagebox.showerror("Error", "Se necesita analizador")
             return
         self.result_text.delete('1.0', tk.END) #vacia la interfaz para el sigueinte resulatado
         with open(filepath, 'r') as file:
@@ -65,7 +65,7 @@ class LexicalApp:
     def display_results(self, results):
         self.result_text.delete('1.0', tk.END)  
         if not results:
-            self.result_text.insert(tk.END, "No tokens found.\n")
+            self.result_text.insert(tk.END, "Tokens no encontrados\n")
         else:
             self.result_text.insert(tk.END, "Tokens encontrados:\n\n") 
             for val, typ in results:
